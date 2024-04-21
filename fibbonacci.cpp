9bc1c726b503dc5e7fe401d8ce2295fb6de20c5d
Mayank Raj <rajmayank7744@gmail.com>
2024-04-22 02:09:18 +0530

    fibbonacci series

---
+++ fibbonacci.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the value of a: ";
+    cin>>a;
+
+    int x=0;
+    int y=1;
+    cout<< x <<" "<< y <<" ";
+
+    for(int i=0; i<=a; i++)
+    {
+       int add= x+y;
+       cout<<add<<" ";
+
+       x=y;
+       y=add;
+
+    }
+    return 0; 
+}
